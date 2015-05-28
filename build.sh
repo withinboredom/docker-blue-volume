f12929698313a2d4e44e3df15942804192ad63b1
Rob Landers <landers.robert@gmail.com>
2015-05-28 00:19:49 -0400

    Updated s6

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-/vagrant/ubuntu-base/build.sh
-/vagrant/consul/build.sh
-/vagrant/consul-ui/build.sh
-/vagrant/consul-agent/build.sh
+#/vagrant/ubuntu-base/build.sh
+#/vagrant/consul/build.sh
+#/vagrant/consul-ui/build.sh
+#/vagrant/consul-agent/build.sh
 
 
-docker build -t withinboredom/blue-volume /vagrant/blue-volume
-docker push withinboredom/blue-volume
+docker build -t withinboredom/blue-volume blue-volume
+#docker push withinboredom/blue-volume
